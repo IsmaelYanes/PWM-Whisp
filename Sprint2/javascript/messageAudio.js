88de317802088a09c347bcaf84ccb21e1787c7b7
Eva <evaayuaan@gmail.com>
2025-03-16 18:52:59 +0000

    Update messageAudio files

--- Sprint2/javascript/messageAudio.js
+++ Sprint2/javascript/messageAudio.js
@@ -1,4 +1,4 @@
-const audio = document.getElementById("player");
+const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
 const playPauseIcon = document.getElementById("playPauseIcon");
 const currentTimeDisplay = document.getElementById("currentTime");
@@ -7,47 +7,48 @@ const durationTimeDisplay = document.getElementById("durationTime");
 function togglePlay() {
     if (audio.paused) {
         audio.play();
-        playPauseIcon.src = "../images/audioStop.png"; // Cambia a icono de pausa
+        playPauseIcon.src = "../images/audioStop.png";
+        requestAnimationFrame(updateProgress);
     } else {
         audio.pause();
-        playPauseIcon.src = "../images/audioPlay.png"; // Vuelve al icono de play
+        playPauseIcon.src = "../images/audioPlay.png";
     }
 }
 
 function updateProgress() {
+    //si el valor de duration del audio no es un num
     if (!isNaN(audio.duration)) {
         progress.value = (audio.currentTime / audio.duration) * 100;
         currentTimeDisplay.textContent = formatTime(audio.currentTime);
     }
-    requestAnimationFrame(updateProgress);
+
+    if (!audio.ended) {
+        requestAnimationFrame(updateProgress);
+    }
 }
 
 function formatTime(seconds) {
     const min = Math.floor(seconds / 60);
     const sec = Math.floor(seconds % 60);
-    return `${min}:${sec < 10 ? "0" : ""}${sec}`;
+    let formattedSeconds = sec < 10 ? "0"+sec : sec;
+    return min + ":" + formattedSeconds;
 }
 
+//se dispara cuando el navegador carga los metadatos de un audio o video
+//garantiza la disponibilidad del valor de duracion del audio
 audio.addEventListener("loadedmetadata", () => {
-    durationTimeDisplay.textContent = formatTime(audio.duration); // Muestra la duración total
-    currentTimeDisplay.textContent = "0:00"; // Asegura que siempre empieza en 0
-});
-
-audio.addEventListener("play", () => {
-    playPauseIcon.src = "../images/audioStop.png";
-    requestAnimationFrame(updateProgress);
-});
-
-audio.addEventListener("pause", () => {
-    playPauseIcon.src = "../images/audioPlay.png";
+    durationTimeDisplay.textContent = formatTime(audio.duration);
+    currentTimeDisplay.textContent = "0:00";
 });
 
 audio.addEventListener("ended", () => {
     playPauseIcon.src = "../images/audioPlay.png";
     progress.value = 0;
+    audio.currentTime = 0;
     currentTimeDisplay.textContent = "0:00";
 });
 
+//actualizar valor si se modifica la barra
 progress.addEventListener("input", () => {
     audio.currentTime = (progress.value / 100) * audio.duration;
 });
