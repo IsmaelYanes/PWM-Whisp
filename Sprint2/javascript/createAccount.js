7783c84471e61e5d1e6b1819a4126ba7bcf5a761
crazy <crazydoufuz@gmail.com>
2025-03-16 13:16:51 +0000

    add post new user

--- Sprint2/javascript/createAccount.js
+++ Sprint2/javascript/createAccount.js
@@ -17,6 +17,24 @@ function loadComponent(url, containerId, callback = null) {
         .catch(error => console.error(error));
 }
 
+function postNewUser(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const username = document.getElementById("username").value;
+    const birthday = document.getElementById("birthday").value;
+    const newUser = {
+        email: email,
+        password: password,
+        username: username,
+        birthday: birthday
+    };
+    fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newUser)
+    }).catch(error => console.error(error));
+}
+
 function addCreateAccountEventListeners() {
     document.querySelectorAll(".toggle-Password").forEach(button => {
         button.addEventListener("click", function () {
@@ -28,11 +46,12 @@ function addCreateAccountEventListeners() {
     document.getElementById("createAccountButton").addEventListener("click", function () {
         const password = document.getElementById("password").value;
         const confirmPassword = document.getElementById("confirm-Password").value;
-
         if (password !== confirmPassword) {
             alert("Las contraseñas no coinciden.");
         } else {
+            postNewUser();
             alert("Cuenta creada con éxito.");
+            window.location.href = "../pages/mainPage.html";
         }
     });
 }
