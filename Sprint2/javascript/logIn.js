47ff36f15ce1faf0910c7f6b7d2dea8ba064927e
Eva <evaayuaan@gmail.com>
2025-03-29 12:53:16 +0000

    Add to logIn.js the password verification

--- Sprint2/javascript/logIn.js
+++ Sprint2/javascript/logIn.js
@@ -28,9 +28,8 @@ function submitExistUser(){
         .then(data => {
             if (data.length > 0) {
                 const user = data[0];
-
-                alert("Inicio de sesión exitoso.");
-                document.getElementById("logIn-Form").submit(); // Envía el formulario si todo está correcto
+                alert("Inicio de sesión exitoso." );
+                document.getElementById("logIn-Form").submit();
                 window.location.href = `../pages/mainPage.html?userID=${user.id}`;
             } else {
                 alert("Usuario no encontrado");
@@ -47,16 +46,16 @@ function addEventListeners() {
     }
 }
 
-function validateLogInForm(event) {
+async function validateLogInForm(event) {
     event.preventDefault(); // Evita el envío si hay errores
 
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    const emailRegex =  /^(?=[^@]*[a-zA-Z])([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    const emailRegex = /^(?=[^@]*[a-zA-Z])([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
     const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[\W_])(?=.{8,}).+$/;
 
-    let isValid = true;
+    let isValid = false;
 
     const emailError = document.getElementById("emailError");
     const emailInput = document.getElementById("email");
@@ -65,38 +64,70 @@ function validateLogInForm(event) {
 
     if (!email) {
         emailError.textContent = "El correo es obligatorio.";
-        emailError.style.display = "block";
-        emailInput.style.border = "2px solid red";
-        isValid = false;
+        basicStyleError(emailError, emailInput);
     } else if (!emailRegex.test(email)) {
         emailError.textContent = "El correo no cumple con el formato válido.";
-        emailError.style.display = "block";
-        emailInput.style.border = "2px solid red";
-        isValid = false;
+        basicStyleError(emailError, emailInput);
+    } else if (!await existUserEmail(email)) {
+        emailError.textContent = "El usuario no existe.";
+        basicStyleError(emailError, emailInput);
     } else {
-        emailError.textContent = "";
-        emailError.style.display = "none";
-        emailInput.style.border = "2px solid #A8A8A8";
+        normalStyle(emailError, emailInput);
     }
 
     if (!password) {
         passwordError.textContent = "La contraseña es obligatoria.";
-        passwordError.style.display = "block";
-        passwordInput.style.border = "2px solid red";
-        isValid = false;
+        basicStyleError(passwordError, passwordInput);
     } else if (!passwordRegex.test(password)) {
         passwordError.textContent = "La contraseña debe tener al menos 8 caracteres, " +
             "incluir una letra mayúscula, una minúscula, un número y un carácter especial.";
-        passwordError.style.display = "block";
-        passwordInput.style.border = "2px solid red";
-        isValid = false;
+        basicStyleError(passwordError, passwordInput);
+    } else if (!await correctPassword(email, password)) {
+        passwordError.textContent = "La contraseña no coincide.";
+        basicStyleError(passwordError, passwordInput);
     } else {
-        passwordError.textContent = "";
-        passwordError.style.display = "none";
-        passwordInput.style.border = "2px solid #A8A8A8";
+        normalStyle(passwordError, passwordInput);
+        isValid = true;
     }
 
     if (isValid) {
         submitExistUser();
+    }
+}
+
+function normalStyle(error, input) {
+    error.textContent = "";
+    error.style.display = "none";
+    input.style.border = "2px solid #A8A8A8";
+}
+
+function basicStyleError(error, input) {
+    error.style.display = "block";
+    input.style.border = "2px solid red";
+}
+
+async function existUserEmail(userEmail) {
+    try {
+        const response = await fetch("http://localhost:3000/users?email=" + userEmail);
+        const data = await response.json();
+        return data.length > 0;
+    } catch {
+        return false;
+    }
+}
+
+async function correctPassword(userEmail, password) {
+    try {
+        const response = await fetch("http://localhost:3000/users?email=" + userEmail);
+        const data = await response.json();
+        if (data.length > 0) {
+            const user = data[0];
+            if (user.password !== password) {
+                return false;
+            }
+        }
+        return true;
+    } catch {
+        return false;
     }
 }
