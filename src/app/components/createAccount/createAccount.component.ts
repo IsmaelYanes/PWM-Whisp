d70dcd1a1a9ed82ee9bcf18a747e815812465ba0
crazy <crazydoufuz@gmail.com>
2025-04-26 21:51:54 +0100

    add loadchat

--- src/app/components/createAccount/createAccount.component.ts
+++ src/app/components/createAccount/createAccount.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Firestore, addDoc, collection} from '@angular/fire/firestore';
+import { UserModel} from '../../models/user.model';
 
 @Component({
   selector: 'app-createAccount',
@@ -10,5 +12,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './createAccount.component.css'
 })
 export class CreateAccountComponent {
+  private db: Firestore = inject(Firestore);
+  private dbRef = collection(this.db, 'user')
 
+  async addUser() {
+    const user: UserModel = {
+      birthday: "01/01/01",
+      email:"hola@.qq.com",
+      password:"12345",
+      profilePhoto:"1.png",
+      userName: "hola"
+    }
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,12 +2,27 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+import { FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    console.log(this.email);
+    this.authService.login(this.email, this.password)
+      .then(() => alert('Sesión iniciada'))
+      .catch(err => alert(err.message));
+  }
+}
